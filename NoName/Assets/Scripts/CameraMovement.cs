920c2aa9550cc31efba903ee5d666bb11d5df84a
artume99 <artume.pichersky@mail.huji.ac.il>
2021-11-30 01:10:36 +0200

    camera+sprites
    add the sprites (no parallax backgground)
    add camera script
    add parralex script (not tested)

--- NoName/Assets/Scripts/CameraMovement.cs
+++ NoName/Assets/Scripts/CameraMovement.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform lookAt;
+
+    [SerializeField] private float boundX = 0.15f;
+    [SerializeField] private float boundY = 0.05f;
+
+    private void Start()
     {
-        
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        Vector3 delta = Vector3.zero;
+        float deltaX = lookAt.position.x - transform.position.x;
+        if (deltaX > boundX || deltaX < -boundX)
+        {
+            if (transform.position.x < lookAt.position.x)
+            {
+                delta.x = deltaX - boundX;
+            }
+            else
+            {
+                delta.x = deltaX + boundX;
+            }
+        }
+
+        float deltaY = lookAt.position.y - transform.position.y;
+        if (deltaY > boundY || deltaY < -boundY)
+        {
+            if (transform.position.y < lookAt.position.y)
+            {
+                delta.y = deltaY - boundY;
+            }
+            else
+            {
+                delta.y = deltaY + boundY;
+            }
+        }
+
+        transform.position += new Vector3(delta.x, delta.y, 0);
     }
 }
---
+++ NoName/Assets/Scripts/ParalaxBackground.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParalaxBackground : MonoBehaviour
+{
+    [SerializeField] private Vector2 parallaxEffectMultiplier = new Vector2(0.5f,0.5f);
+    
+    private Transform cameraTransform;
+    private Vector3 lastCameraPoistion;
+    private float textureUnitSizeX;
+    // private float textureUnitSizeY; // incase we want to use inf. cave upwards
+    
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraPoistion = cameraTransform.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
+        // textureUnitSizeY = texture.height / sprite.pixelsPerUnit;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 deltaMove = cameraTransform.position - lastCameraPoistion;
+        transform.position += new Vector3(deltaMove.x * parallaxEffectMultiplier.x, deltaMove
+            .y * parallaxEffectMultiplier.y, 0);
+        lastCameraPoistion = cameraTransform.position;
+        if (Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
+        {
+            float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+            transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y);
+        }
+        
+        // if (Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
+        // {
+        //     float offsetPositionY = (cameraTransform.position.y - transform.position.y) % textureUnitSizeY;
+        //     transform.position = new Vector3(cameraTransform.position.x , transform.position.y + offsetPositionY);
+        // }
+    }
+}
